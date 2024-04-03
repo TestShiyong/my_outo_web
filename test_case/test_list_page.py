bafa706f9fbcbae4021b9c0c22a2a8fb0939c92a
shiyong <shiyong@gaoyaya.com>
2024-04-03 11:50:07 +0800

    T

--- test_case/test_list_page.py
+++ test_case/test_list_page.py
@@ -1,54 +1,93 @@
 import time
 
+import pytest
 import base_path
-from pages import all_page_url
+from common.basepage import BasePage
+from pages import all_page_url as al_page
 from common.handle_images import run_comparison
 
-screenshots_path = base_path.screenshots_path
 
+class TestPageComparison(BasePage):
+    screenshots_path = base_path.screenshots_path
 
+    @pytest.mark.parametrize('page_data', al_page.get_bd_urls())
+    def test_compare_bd_list(self, page_data):
+        try:
+            run_comparison(page_data['pro_url'], page_data['pre_url'], page_data['pre_list_screenshots_path'],
+                           page_data['pre_detail_screenshots_path'], page_data['pro_list_screenshots_path'],
+                           page_data['pro_detail_screenshots_path'], page_data['list_diff_image_path'],
+                           page_data['detail_diff_image_path']
+                           )
+        except Exception as e:
+            print("Exception occurred:", e)
 
-def test_compare_bd_list():
-    list_page_dates = all_page_url.get_bd_urls()
-    for page_date in list_page_dates:
-        run_comparison(page_date['pro_url'], page_date['pre_url'], page_date['pre_list_screenshots_path'],
-                       page_date['pre_detail_screenshots_path'], page_date['pro_list_screenshots_path'],
-                       page_date['pro_detail_screenshots_path'], page_date['list_diff_image_path'],
-                       page_date['detail_diff_image_path']
-                       )
-    input('ccccccc')
-
-#
-# def test_compare_jbd_list():
-#     page_date = all_page_url.get_jbd_urls()
-#     run_comparison(page_date['page_name'], page_date['pro_url'], page_date['pre_url'], page_date['page_type'],
-#                    screenshots_path)
-#
-#
-# def test_compare_sod_list():
-#     page_date = all_page_url.get_sod_urls()
-#     run_comparison(page_date['page_name'], page_date['pro_url'], page_date['pre_url'], page_date['page_type'],
-#                    screenshots_path)
-#
-#
-# def test_compare_acc_list():
-#     page_date = all_page_url.get_acc_urls()
-#     run_comparison(page_date['page_name'], page_date['pro_url'], page_date['pre_url'], page_date['page_type'],
-#                    screenshots_path)
-#
-#
-# def test_compare_sample_list():
-#     page_date = all_page_url.get_sample_urls()
-#     run_comparison(page_date['page_name'], page_date['pro_url'], page_date['pre_url'], page_date['page_type'],
-#                    screenshots_path)
-#
-#
-# def test_compare_swatch_list():
-#     page_date = all_page_url.get_swatch_urls()
-#     run_comparison(page_date['page_name'], page_date['pro_url'], page_date['pre_url'], page_date['page_type'],
-#                    screenshots_path)
-#
-# def test_compare_rts_list():
-#     page_date = all_page_url.get_rts_urls()
-#     run_comparison(page_date['page_name'], page_date['pro_url'], page_date['pre_url'], page_date['page_type'],
-#                    screenshots_path)
+    # @pytest.mark.parametrize('page_data', al_page.get_jbd_urls())
+    # def test_compare_jbd_list(page_data):
+    #     try:
+    #         run_comparison(page_data['pro_url'], page_data['pre_url'], page_data['pre_list_screenshots_path'],
+    #                        page_data['pre_detail_screenshots_path'], page_data['pro_list_screenshots_path'],
+    #                        page_data['pro_detail_screenshots_path'], page_data['list_diff_image_path'],
+    #                        page_data['detail_diff_image_path']
+    #                        )
+    #     except Exception as e:
+    #         print("Exception occurred:", e)
+    #
+    #
+    # @pytest.mark.parametrize('page_data', al_page.get_sod_urls())
+    # def test_compare_sod_list(page_data):
+    #     try:
+    #         run_comparison(page_data['pro_url'], page_data['pre_url'], page_data['pre_list_screenshots_path'],
+    #                        page_data['pre_detail_screenshots_path'], page_data['pro_list_screenshots_path'],
+    #                        page_data['pro_detail_screenshots_path'], page_data['list_diff_image_path'],
+    #                        page_data['detail_diff_image_path']
+    #                        )
+    #     except Exception as e:
+    #         print("Exception occurred:", e)
+    #
+    #
+    # @pytest.mark.parametrize('page_data', al_page.get_acc_urls())
+    # def test_compare_acc_list(page_data):
+    #     try:
+    #         run_comparison(page_data['pro_url'], page_data['pre_url'], page_data['pre_list_screenshots_path'],
+    #                        page_data['pre_detail_screenshots_path'], page_data['pro_list_screenshots_path'],
+    #                        page_data['pro_detail_screenshots_path'], page_data['list_diff_image_path'],
+    #                        page_data['detail_diff_image_path']
+    #                        )
+    #     except Exception as e:
+    #         print("Exception occurred:", e)
+    #
+    #
+    # @pytest.mark.parametrize('page_data', al_page.get_sample_urls())
+    # def test_compare_sample_list(page_data):
+    #     try:
+    #         run_comparison(page_data['pro_url'], page_data['pre_url'], page_data['pre_list_screenshots_path'],
+    #                        page_data['pre_detail_screenshots_path'], page_data['pro_list_screenshots_path'],
+    #                        page_data['pro_detail_screenshots_path'], page_data['list_diff_image_path'],
+    #                        page_data['detail_diff_image_path']
+    #                        )
+    #     except Exception as e:
+    #         print("Exception occurred:", e)
+    #
+    #
+    # @pytest.mark.parametrize('page_data', al_page.get_swatch_urls())
+    # def test_compare_swatch_list(page_data):
+    #     try:
+    #         run_comparison(page_data['pro_url'], page_data['pre_url'], page_data['pre_list_screenshots_path'],
+    #                        page_data['pre_detail_screenshots_path'], page_data['pro_list_screenshots_path'],
+    #                        page_data['pro_detail_screenshots_path'], page_data['list_diff_image_path'],
+    #                        page_data['detail_diff_image_path']
+    #                        )
+    #     except Exception as e:
+    #         print("Exception occurred:", e)
+    #
+    #
+    # @pytest.mark.parametrize('page_data', al_page.get_rts_urls())
+    # def test_compare_rts_list(page_data):
+    #     try:
+    #         run_comparison(page_data['pro_url'], page_data['pre_url'], page_data['pre_list_screenshots_path'],
+    #                        page_data['pre_detail_screenshots_path'], page_data['pro_list_screenshots_path'],
+    #                        page_data['pro_detail_screenshots_path'], page_data['list_diff_image_path'],
+    #                        page_data['detail_diff_image_path']
+    #                        )
+    #     except Exception as e:
+    #         print("Exception occurred:", e)
